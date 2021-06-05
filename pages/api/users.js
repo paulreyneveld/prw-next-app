2d773bbf5f540e05c184ded575d69aec7d028c27
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-04 19:52:36 -0700

    Hid secret key

--- pages/api/users.js
+++ pages/api/users.js
@@ -3,10 +3,10 @@ const assert = require('assert');
 const bcrypt = require('bcrypt');
 const v4 = require('uuid').v4;
 const jwt = require('jsonwebtoken');
-const jwtSecret = 'SUPERSECRETE20220';
+const jwtSecret = process.env.SECRET_KEY;
 
 const saltRounds = 10;
-const url = 'mongodb://localhost:27017';
+const url = process.env.DB_URL;
 const dbName = 'simple-login-db';
 
 const client = new MongoClient(url, {
