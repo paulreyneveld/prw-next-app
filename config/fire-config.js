cb86ab89a30b8ba570d2d144be2b1cfaae337b36
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-14 21:22:41 -0700

    Finally got the config file set such that the connection the Firebase connection works as expected

--- config/fire-config.js
+++ config/fire-config.js
@@ -3,7 +3,7 @@ import firebase from 'firebase';
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
+  projectId: "blogapp-c121b",
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID
--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ const Home = () => {
         setBlogs(blogs);
       });
   }, []);
+  
   return (
     <div>
       <Head>
