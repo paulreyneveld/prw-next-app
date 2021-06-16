c3c676ba0bbb6d0743f97841a36dc3a03f4bc4af
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-16 16:38:39 -0700

    Updated [id].js to appropriately collect the data via getServerSideProps

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,30 +1,12 @@
-import { useEffect, useState } from 'react';
+// pages/blog/[id].js
 import fire from '../../config/fire-config';
 import Link from 'next/link'
 const Blog = (props) => {
-  const [blog, setBlog] = useState(null);
-
-  useEffect(() => {
-    fire.firestore()
-      .collection('blog')
-      .doc(props.id)
-      .get()
-      .then(result => {
-        setBlog(result.data())
-      })
-  }, []);
-
-  if(!blog){
-    return(
-      <h2>Loading...</h2>
-    )
-  }
-
   return (
     <div>
-      <h2>{blog.title}</h2>
+      <h2>{props.title}</h2>
       <p>
-        {blog.content}
+        {props.content}
       </p>
       <Link href="/">
         <a>Back</a>
@@ -32,25 +14,21 @@ const Blog = (props) => {
     </div>
   )
 }
-
 export const getServerSideProps = async ({ query }) => {
-    const content = {};
-
-    await fire.firestore()
-      .collection('blog')
-      .doc(query.id)
-      .get()
-      .then(result => {
-        content['title'] = result.data().title;
-        content['content'] = result.data().content;
-      });
-      
-  return {
-      props: {
-        title: content.title,
-        content: content.content,
-      }
+  const content = {}
+  await fire.firestore()
+    .collection('blog')
+    .doc(query.id)
+    .get()
+    .then(result => {
+      content['title'] = result.data().title;
+      content['content'] = result.data().content;
+    });
+return {
+    props: {
+      title: content.title,
+      content: content.content,
     }
   }
-
-export default Blog;
+}
+export default Blog
