c764de2e6ffdddc667c801674a65afea9d505f2e
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-16 16:35:07 -0700

    Added a next.config.js to allow for client access to certain env variables

--- config/fire-config.js
+++ config/fire-config.js
@@ -4,7 +4,7 @@ import firebase from 'firebase';
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  projectId: "blogapp-c121b",
+  projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID
---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    env: {
+        PROJECT_ID: process.env.PROJECT_ID,
+        API_KEY: process.env.API_KEY,
+    },
+};
