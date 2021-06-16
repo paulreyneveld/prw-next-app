c8ad19868f23a39eebe6df1b66006e5e5fd248f2
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-15 18:34:19 -0700

    Updated [id].js

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,16 +1,39 @@
-const Blog = () => {
-
-    Blog.getInitialProps = ({ query }) => {
-        return {
-            id: query.id,
-        }
-    }
-
-    return (
-      <div>
-        Some blog
-      </div>
+import { useEffect, useState } from 'react';
+import fire from '../../config/fire-config';
+import Link from 'next/link'
+const Blog = (props) => {
+  const [blog, setBlog] = useState(null);
+  useEffect(() => {
+    fire.firestore()
+      .collection('blog')
+      .doc(props.id)
+      .get()
+      .then(result => {
+        setBlog(result.data())
+      })
+  }, []);
+  if(!blog){
+    return(
+      <h2>Loading...</h2>
     )
   }
+  return (
+    <div>
+      <h2>{blog.title}</h2>
+      <p>
+        {blog.content}
+      </p>
+      <Link href="/">
+        <a>Back</a>
+      </Link>
+    </div>
+  )
+}
 
-  export default Blog;
+Blog.getInitialProps = ({ query }) => {
+  return {
+      id: query.id,
+  }
+}
+
+export default Blog;
