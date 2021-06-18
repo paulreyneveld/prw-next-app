0a30042622bf7025041ef18861d11fbc6aa01cb2
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-18 16:58:39 -0700

    Updated Header and Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,30 +1,54 @@
+import Head from "next/head";
 import Header from "./Header";
 import NavBar from "./NavBar";
-import Head from "next/head";
-
-const layoutStyle = {
-  display: "flex",
-  flexDirection: "column",
-  height: "100%",
-  width: "100%"
-};
-
-const contentStyle = {
-  flex: 1,
-  display: "flex",
-  flexDirection: "column"
-};
 
 const Layout = props => (
-  <div className="Layout" style={layoutStyle}>
-      <Head>
-    <title>PRW</title>
-  </Head>
+  <div className="Layout">
+    <Head>
+      <title>WHATABYTE</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+    </Head>
+
     <Header />
-    <div className="Content" style={contentStyle}>
-      {props.children}
-    </div>
+    <div className="Content">{props.children}</div>
     <NavBar />
+    <style jsx global>{`
+      * {
+        box-sizing: border-box;
+      }
+
+      html,
+      body,
+      #__next {
+        height: 100%;
+        width: 100%;
+      }
+
+      body {
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+          "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+          "Helvetica Neue", sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+
+      .Layout {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        width: 100%;
+      }
+
+      .Content {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        font-family: Arial;
+      }
+    `}</style>
   </div>
 );
 
