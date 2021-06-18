37853fc76f198ee28af0de3da3dab9ab8d0db043
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-18 16:52:04 -0700

    Moved head into Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import NavBar from "./NavBar";
+import Head from "next/head";
 
 const layoutStyle = {
   display: "flex",
@@ -16,6 +17,9 @@ const contentStyle = {
 
 const Layout = props => (
   <div className="Layout" style={layoutStyle}>
+      <Head>
+    <title>PRW</title>
+  </Head>
     <Header />
     <div className="Content" style={contentStyle}>
       {props.children}
