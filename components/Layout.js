b46bbdccee03cf6ece10fbc3dd0fb010b7db2e2d
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-19 16:56:32 -0700

    Updated title

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import NavBar from "./NavBar";
 const Layout = props => (
   <div className="Layout">
     <Head>
-      <title>WHATABYTE</title>
+      <title>PRW</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
     </Head>
