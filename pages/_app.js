35289d6533218440a2fa04c049a750497e17a40c
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-20 16:16:00 -0700

    Added styling to Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import "../components/Header.scss";
 import "../components/NavBar.scss";
+import "../components/Layout.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
