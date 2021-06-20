46f47d2ec747ded8dcfbf8fc256faa9b81e96a91
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-20 16:11:52 -0700

    Added scss import to _app

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,7 @@
 const Header = () => (
     <div className="Header">
       HEADER
-      <style jsx>{`
-        background-color: blue;
-        color: white;
-        width: 100%;
-        height: 50px;
-      `}</style>
     </div>
   );
   
-  export default Header;
+export default Header;
