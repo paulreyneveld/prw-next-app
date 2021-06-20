a8a05e3a60e8f7cdd6eea634dc5d72dfde373e35
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-20 16:18:08 -0700

    Added an index scss file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css';
-import "../components/Header.scss";
-import "../components/NavBar.scss";
-import "../components/Layout.scss";
+import '../components/Header.scss';
+import '../components/NavBar.scss';
+import '../components/Layout.scss';
+import '../pages/index.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
