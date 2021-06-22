eb72d78b639ab14cbdbe4765eeb218e6d2bcf7b3
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-22 16:19:21 -0700

    Updated Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,25 @@
 import Head from "next/head";
 import Header from "./Header";
 import NavBar from "./NavBar";
+import AdminNavBar from "./AdminNavBar";
 
-const Layout = props => (
+const Layout = props => {
+
+
+  return (
   <div className="Layout">
     <Head>
       <title>PRW</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
     </Head>
-
     <Header />
     <NavBar />
+    <AdminNavBar />
     <div className="Content">{props.children}</div>
     
   </div>
 );
+}
 
 export default Layout;
