53f9cfd88ee513927058f9cdc21b224abf37af0b
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-22 16:15:51 -0700

    Updated NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,9 +2,17 @@ import Link from 'next/link';
   
 const NavBar = () => (
     <div className="NavBar">
-      NAVBAR
-      <Link href="/blog/newblog">
-          <a>Create Post</a>
+      <Link href="/blog">
+          <a>Blog</a>
+      </Link>
+      <Link href="/about">
+          <a>About</a>
+      </Link>
+      <Link href="/projects">
+          <a>Projects</a>
+      </Link>
+      <Link href="/resources">
+          <a>Blog</a>
       </Link>
     </div>
 );
